95b460a3dbf20a5bc437ee43170624a0708f3fa0
stephtheitsloth <stephan.karim.sk@gmail.com>
2022-10-09 18:55:56 -0400

    frontend file

---
+++ src/frontend/app.js
@@ -0,0 +1,13 @@
+
+const msg = document.querySelector('#msg')
+const msgDisplay = document.querySelector('.msg-view')
+const formButton = document.querySelector('.form-btn')
+
+//implement post new msg
+
+
+//implement get new msg
+
+
+//a render function
+
